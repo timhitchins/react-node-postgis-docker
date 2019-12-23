a0db86406ab4c3d148823d976ff74b5380a6905e
Tim Hitchins <tim.x.hitchins@gmail.com>
2019-12-23 13:36:42 -0800

    set up initial docker node app and postgis db

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 
 const client = new Client({
   user: "ppraxis",
-  host: "localhost",
-  database: "db",
+  host: "postgis",
+  database: "postgres",
   password: "testing",
   port: 35432
 });
@@ -13,3 +13,12 @@ client.query("SELECT NOW()", (err, res) => {
   console.log(err, res);
   client.end();
 });
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello World!")
+})
+
+app.listen(5000, ()=>{
+    console.log("listening on port 5000")
+})
